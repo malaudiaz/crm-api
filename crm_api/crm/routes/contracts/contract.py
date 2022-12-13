28af3d22dae29866fa9623cdec9b7a92a089d2a2
migue <miraidys.garcia@etecsa.cu>
2022-12-13 09:32:29 -0500

    Cambios en el paginado de los contratos

--- crm_api/crm/routes/contracts/contract.py
+++ crm_api/crm/routes/contracts/contract.py
@@ -3,7 +3,7 @@ from ...schemas.contracts.contracts import ContractBase, ContractShema
 from sqlalchemy.orm import Session
 from ...app import get_db
 from typing import List, Dict
-from ...services.contrats.contract import get_all, new, get_one, delete, update, get_by_number
+from ...services.contrats.contract import get_all, new, get_one, delete, update
 from starlette import status
 from ...auth_bearer import JWTBearer
 import uuid
@@ -15,20 +15,17 @@ contract_route = APIRouter(
 
 @contract_route.get("/contracts", response_model=Dict, summary="Obtener lista de Contratos")
 def get_contracts(
-    totalCount: int = 0,
-    skip: int = 0, 
-    limit: int = 100, 
+    page: int = 1, 
+    per_page: int = 6, 
+    criteria_key: str = "",
+    criteria_value: str = "",
     db: Session = Depends(get_db)
 ):
-    return get_all(totalCount=totalCount, skip=skip, limit=limit, db=db)
+    return get_all(page=page, per_page=per_page, criteria_key=criteria_key, criteria_value=criteria_value, db=db)
 
 @contract_route.get("/contracts/{id}", response_model=ContractShema, summary="Obtener un Contrato por su ID")
 def get_contract_by_id(id: str, db: Session = Depends(get_db)):
     return get_one(contract_id=id, db=db)
-
-@contract_route.get("/contracts/number/{number}", response_model=ContractShema, summary="Obtener un Contrato por su numero de contrato")
-def get_contract_by_number(number: str, db: Session = Depends(get_db)):
-    return get_by_number(number=number, db=db)
 
 @contract_route.post("/contracts", response_model=ContractShema, summary="Crear un Contrato")
 def create_contract(contract: ContractBase, db: Session = Depends(get_db)):

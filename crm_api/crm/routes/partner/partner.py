14e88624339702e5a51f9ca3c7dec7fe4432cc23
migue <miraidys.garcia@etecsa.cu>
2022-12-27 12:28:39 -0500

    Actualizando contactos de clientes

--- crm_api/crm/routes/partner/partner.py
+++ crm_api/crm/routes/partner/partner.py
@@ -34,19 +34,18 @@ def get_partner_by_id(id: str, db: Session = Depends(get_db)):
 def get_partner_by_registration_number(registration_number: str, db: Session = Depends(get_db)):
     return get_one_by_registration_number(registration_number=registration_number, db=db)
 
-# @partner_route.post("/partners", response_model=PartnerShema, summary="Crear un Cliente")
 @partner_route.post("/partners", summary="Crear un Cliente")
 def create_partner(request:Request, partner: PartnerBase, db: Session = Depends(get_db)):
     return new(request=request, partner=partner, db=db)
 
 @partner_route.delete("/partners/{id}", status_code=status.HTTP_200_OK, summary="Desactivar un Cliente por su ID")
-def delete_partner(id: uuid.UUID, db: Session = Depends(get_db)):
-    is_delete = delete(partner_id=str(id), db=db)
+def delete_partner(request:Request, id: uuid.UUID, db: Session = Depends(get_db)):
+    is_delete = delete(request=request, partner_id=str(id), db=db)
     if is_delete:
         raise HTTPException(status_code=200, detail="Cliente Desactivado")
     else:
         raise HTTPException(status_code=404, detail="Cliente no encontrado")
 
-@partner_route.put("/partners/{id}", response_model=PartnerShema, summary="Actualizar un Cliente por su ID")
-def update_partner(id: uuid.UUID, partner: PartnerBase, db: Session = Depends(get_db)):
-    return update(db=db, partner_id=str(id), partner=partner)
+@partner_route.put("/partners/{id}", summary="Actualizar un Cliente por su ID")
+def update_partner(request:Request, id: str, partner: PartnerBase, db: Session = Depends(get_db)):
+    return update(request=request, partner_id=id, partner=partner, db=db)

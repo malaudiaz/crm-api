c533ecede5fee2cf30efba93efcbdef5574c8518
migue <miraidys.garcia@etecsa.cu>
2022-12-29 17:11:00 -0500

    Devolviendo id de conoacto y partner

--- crm_api/crm/services/contrats/contract.py
+++ crm_api/crm/services/contrats/contract.py
@@ -23,8 +23,8 @@ def get_all(page: int, per_page: int, criteria_key: str, criteria_value: str, db
         "JOIN resources.status_element st ON st.name = cont.status_name " \
         "LEFT JOIN enterprise.users us ON us.id = cont.sign_by "
     str_count = "Select count(*) FROM contract.contracts cont"
-    str_query = "Select cont.id, number, pa.name as partner_name, co.name as contact_name, sign_by,  us.fullname as sign_full_name, " \
-        "sign_date, initial_aproved_import, real_aproved_import, real_import, " \
+    str_query = "Select cont.id, number, pa.id as partner_id, pa.name as partner_name, co.name as contact_name, co.id as contact_id," \
+        "sign_by,  us.fullname as sign_full_name, sign_date, initial_aproved_import, real_aproved_import, real_import, " \
         "is_supplement, contract_id, cont.created_date, status_name, st.description as status_description " \
         "FROM contract.contracts cont "
     
@@ -49,8 +49,8 @@ def get_all(page: int, per_page: int, criteria_key: str, criteria_value: str, db
     lst_data = db.execute(str_query)
     data = []
     for item in lst_data:
-        data.append({'id': item['id'], 'number' : item['number'], 'partner': item['partner_name'], 
-                     'contact': item['contact_name'], 'sign_by': item['sign_by'], 'sign_full_name': item['sign_full_name'], 
+        data.append({'id': item['id'], 'number' : item['number'], 'partner': item['partner_name'], 'partner_id': item['partner_id'], 
+                     'contact': item['contact_name'], 'contact_id': item['contact_id'], 'sign_by': item['sign_by'], 'sign_full_name': item['sign_full_name'], 
                      'sign_date': item['sign_date'], 
                      'initial_aproved_import': item['initial_aproved_import'], 'real_aproved_import': item['real_aproved_import'],  
                      'real_import': item['real_import'], 'selected': False, 'status_name': item['status_name'], 
